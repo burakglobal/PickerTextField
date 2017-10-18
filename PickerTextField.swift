f2e9e24251348d643b6ec0f69709c5efd70ac949
Djuro Alfirevic <djalfirevic@gmail.com>
2017-10-18 21:36:14 +0200

    Update PickerTextField.swift
    Small code refactoring

--- PickerTextField.swift
+++ PickerTextField.swift
@@ -16,7 +16,7 @@ protocol PickerTextFieldDelegate: class {
 }
 
 class PickerTextField: UITextField {
-    
+
     // MARK: - Properties
     var options = [String]() {
         didSet {
@@ -48,68 +48,68 @@ class PickerTextField: UITextField {
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         setup()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+
         setup()
     }
-    
+
     // MARK: - Actions
     @objc func pickerDone() {
         guard let option = selectedOption else {
             return
         }
-        
+
         text = selectedOption
-        
+
         if let pickerDelegate = pickerDelegate {
             pickerDelegate.pickerTextField(self, didSelectOption: option)
         }
-        
+
         endEditing(true)
     }
-    
+
     @objc func datePickerValueChanged(sender: UIDatePicker) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM/dd/yyyy"
         text = dateFormatter.string(from: date)
         selectedOption = text
         date = sender.date
-        
+
         if let pickerDelegate = pickerDelegate {
             pickerDelegate.pickerTextField(self, didSelectOption: dateFormatter.string(from: date))
         }
     }
-    
+
     // MARK: - Private API
     private func setup() {
         let pickerView = createPicker()
         inputView = pickerView
-        
+
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(pickerDone))
         inputAccessoryView = createToolbar(with: [flexibleSpace, doneButton])
-        
+
         if let text = selectedOption {
             var index = Int(INT_MAX)
-            
+
             if options.contains(text) {
                 index = options.index(of: text)!
             }
-            
+
             if index != Int(INT_MAX) {
                 pickerView.selectRow(index, inComponent: 0, animated: true)
             }
         }
     }
-    
+
     private func setupDatePicker() {
         inputView  = createDatePicker()
-        
+
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(pickerDone))
         inputAccessoryView = createToolbar(with: [flexibleSpace, doneButton])
@@ -135,7 +135,7 @@ class PickerTextField: UITextField {
         
         return pickerView
     }
-    
+
     private func createDatePicker() -> UIDatePicker {
         let datePicker = UIDatePicker()
         datePicker.datePickerMode = .date
@@ -150,27 +150,27 @@ class PickerTextField: UITextField {
 }
 
 extension PickerTextField: UIPickerViewDataSource, UIPickerViewDelegate {
-    
+
     // MARK: - UIPickerViewDataSource
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return options.count
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
         let option = options[row]
         let attributes = [NSAttributedStringKey.font:UIFont.systemFont(ofSize: 14.0), NSAttributedStringKey.foregroundColor:UIColor.black]
         
         return NSAttributedString(string: option, attributes: attributes)
     }
-    
+
     // MARK: - UIPickerViewDelegate
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let option = options[row]
-        
+
         text = option
         selectedOption = option
         selectedIndex = row
